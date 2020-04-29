a5a8f3ce0e0e8be0081083913bdb71f9ad7d6e79
Matthew <matthew@edge.app>
2020-04-29 14:48:46 -0700

    Add Bity monthly reporting

--- src/bity.js
+++ src/bity.js
@@ -1,7 +1,6 @@
 // @flow
 import type { StandardTx, SwapFuncParams } from './checkSwapService.js'
 const bns = require('biggystring')
-const fs = require('fs')
 const { checkSwapService } = require('./checkSwapService.js')
 const js = require('jsonfile')
 const fetch = require('node-fetch')
@@ -9,7 +8,6 @@ const confFileName = './config.json'
 const config = js.readFileSync(confFileName)
 
 const BITY_CACHE = './cache/bityRaw.json'
-const BITY_FOLDER = './cache/bity'
 const BITY_TOKEN_URL = 'https://connect.bity.com/oauth2/token'
 const BITY_API_URL = 'https://reporting.api.bity.com/exchange/v1/summary/monthly/'
 
@@ -21,55 +19,22 @@ async function doBity (swapFuncParams: SwapFuncParams) {
   )
 }
 
+let queryYear = '2020'
+let queryMonth = '1'
+const todayMonth = bns.add(new Date().getMonth().toString(), '1')
+const todayYear = new Date().getFullYear().toString()
+
 async function fetchBity (swapFuncParams: SwapFuncParams) {
   if (!swapFuncParams.useCache) {
     console.log('Fetching Bity from JSON...')
   }
-  const diskCache = { txs: [] }
-
-  const transactionMap = {}
-  const ssFormatTxs: Array<StandardTx> = []
-
-  const files = await fs.readdirSync(BITY_FOLDER)
-  // console.log('files: ', files)
-
-  for (const fileName of files) {
-    const filePath = `./cache/bity/${fileName}`
-    // console.log('filePath is: ', filePath)
-    const jsonData = js.readFileSync(filePath)
-
-    if (jsonData.txs.length < 1) {
-      break
-    }
-
-    for (const order of jsonData.orders) {
-      if (!order.input.amount || !order.output.amount || !order.timestamp_executed) {
-        continue
-      }
-      const date = new Date(order.timestamp_executed + 'Z')
-      const timestamp = date.getTime() / 1000
-      const uniqueIdentifier = `${order.id}`
-      const inputAmount = Number(order.input.amount)
-      const outputAmount = Number(order.output.amount)
-      const ssTx: StandardTx = {
-        status: 'complete',
-        inputTXID: uniqueIdentifier,
-        inputAddress: '',
-        inputCurrency: order.input.currency,
-        inputAmount,
-        outputAddress: '',
-        outputCurrency: order.output.currency,
-        outputAmount: outputAmount.toString(),
-        timestamp: timestamp
-      }
-      // console.log('ssTx: ', ssTx)
-      transactionMap[uniqueIdentifier] = ssTx
-    }
-  }
-  for (const id in transactionMap) {
-    ssFormatTxs.push(transactionMap[id])
-    ssFormatTxs.sort((a, b) => a.timestamp - b.timestamp)
-  }
+  let diskCache = { txs: [] }
+  try {
+    diskCache = js.readFileSync(BITY_CACHE)
+    // First tx in diskCache is the most recent
+    queryMonth = new Date(diskCache.txs[0].timestamp * 1000).getMonth().toString()
+    queryYear = new Date(diskCache.txs[0].timestamp * 1000).getFullYear().toString()
+  } catch (e) {}
 
   // Get auth token
   const credentials = {
@@ -93,48 +58,45 @@ async function fetchBity (swapFuncParams: SwapFuncParams) {
   const tokenReply = await tokenResponse.json()
   const authToken = tokenReply.access_token
 
-  // Get monthly orders
+  // Query monthly orders
   const newTransactions = []
 
-  let queryYear = '2019'
-  let queryMonth = '9'
   let keepQuerying = true
+  let page = 1
 
   while (keepQuerying) {
-    console.log(`Date query = ${queryYear}-${queryMonth}`)
-    const monthlyResponse = await fetch(`${BITY_API_URL}${queryYear}-${queryMonth}/orders`,
+    const monthlyResponse = await fetch(`${BITY_API_URL}${queryYear}-${queryMonth}/orders?page=${page}`,
       {
         method: 'GET',
         headers: {Authorization: `Bearer ${authToken}`}
       }
     )
     const monthlyTxs = await monthlyResponse.json()
-    // remove this length check 
-    // look into pagination
-    if (monthlyTxs.length === 0) {
-      keepQuerying = false
+
+    for (const tx of monthlyTxs) {
+      const date = new Date(tx.timestamp_executed)
+      const timestamp = date.getTime() / 1000
+
+      const ssTx: StandardTx = {
+        status: 'complete',
+        inputTXID: '',
+        inputAddress: '',
+        inputCurrency: tx.input.currency.toUpperCase(),
+        inputAmount: tx.input.amount,
+        outputAddress: '',
+        outputCurrency: tx.output.currency.toUpperCase(),
+        outputAmount: tx.output.amount.toString(),
+        timestamp
+      }
+      newTransactions.push(ssTx)
     }
 
-    if (keepQuerying) {
-      for (const tx of monthlyTxs) {
-        const date = new Date(tx.timestamp_executed)
-        const timestamp = date.getTime() / 1000
-
-        const ssTx: StandardTx = {
-          status: 'complete',
-          inputTXID: '',
-          inputAddress: '',
-          inputCurrency: tx.input.currency.toUpperCase(),
-          inputAmount: tx.input.amount,
-          outputAddress: '',
-          outputCurrency: tx.output.currency.toUpperCase(),
-          outputAmount: tx.output.amount.toString(),
-          timestamp
-        }
-        newTransactions.push(ssTx)
-      }
-
-      // Add month to queryMonth
+    if (monthlyTxs.length < 100 && queryMonth === todayMonth && queryYear === todayYear) {
+      keepQuerying = false
+    } else if (monthlyTxs.length === 100) {
+      page += 1
+    } else {
+      page = 1
       if (bns.lt(queryMonth, '12')) {
         queryMonth = bns.add(queryMonth, '1')
       } else {
@@ -142,8 +104,6 @@ async function fetchBity (swapFuncParams: SwapFuncParams) {
         queryYear = bns.add(queryYear, '1')
       }
     }
-
-    // console.log('newTransactions is: ', newTransactions)
   }
 
   const out = {
