c9b95607fb9cf8547bdac8d83ecbb5a7d4b01c3f
Paul Puey <paul@edge.app>
2020-08-12 11:42:02 -0700

    update wyre

--- src/wyre.js
+++ src/wyre.js
@@ -32,9 +32,10 @@ function parseTxStr (txStr) {
     completedAt: txItems[4],
     sourceAmount: txItems[5],
     sourceCurrency: txItems[6],
-    destAmount: txItems[7],
-    destCurrency: txItems[8],
-    failureReason: txItems[9]
+    usdFeeEquiv: txItems[7],
+    destAmount: txItems[8],
+    destCurrency: txItems[9],
+    usdEquiv: txItems[10]
   }
 }
 
